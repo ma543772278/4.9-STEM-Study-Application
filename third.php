b24b2f7ecc3f31bc11d691414d5bf83b4e92e4f1
bohan12 <Wang670792079@gmail.com>
2018-08-06 16:54:41 +1200

    Add files via upload

---
+++ third.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+<head>
+ <title>third Page</title>
+</head>
+<h1>thirdpage</h1>
+<?php
+echo "this is the third page";
+?> 
+
+<p><a href="fourth.php">fourth page </a> <br /> - Went to fourth page</p>
+
+<form action="second.php">
+<button type="submit" formaction = "second.php">second page</button>
+</form>
+
+</body>
+</html>
