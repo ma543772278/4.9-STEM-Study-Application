7fa4fda632645a71d1a0262d73683c99a8312ef7
bohan12 <Wang670792079@gmail.com>
2018-08-06 16:46:49 +1200

    Add files via upload

---
+++ R&D/fifth.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+<head>
+ <title>fifth Page</title>
+</head>
+<h1>fifth page</h1>
+<?php
+echo "this is the fifth page";
+?> 
+
+<p><a href="sixth.php">sixth page </a> <br /> - Went to sixth page</p>
+
+<form action="fourth.php">
+<button type="submit" formaction = "fourth.php">fourth page</button>
+</form>
+
+</body>
+</html>
---
+++ R&D/sixth.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+<head>
+ <title>sixth Page</title>
+</head>
+<h1>sixth page</h1>
+<?php
+echo "this is the sixth page";
+?> 
+
+<p><a href="fifth.php">fifth page </a> <br /> - Went to fifth page</p>
+<form action="fifth.php">
+<button type="submit" formaction = "fifth.php">fifth page</button>
+</form>
+
+</body>
+</html>
